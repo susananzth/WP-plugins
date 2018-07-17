5c01666b6fb2c9deee4d132cbb4b02e1e7087d2b
Susana Piñero <susananzth@gmail.com>
2018-07-17 16:07:21 -0500

    Modificando el footer

--- plantilla/footer.php
+++ plantilla/footer.php
@@ -1,10 +1,13 @@
-	<footer id="contact">
-		<div class="flex-box">
-			
-			<address class="contact text-center">
-				<p title="E-mail" class="icons-footer email"> <a href="mailto:soporte@winhold.net?Subject=Contacto" target="_top">soporte@winhold.net</a></p>
-			</address>	
+	<footer>
+		<div class="text-center">
+			<figure class="logo-app"><img src="img/app.png"></figure>
+			<figure class="logo-app"><img src="img/play.png"></figure>
 		</div>
+		
+		<address class="contact text-center">
+			<p title="E-mail" class="icons-footer email"><a href="mailto:soporte@winhold.net?Subject=Contacto" target="_top">soporte@winhold.net</a></p>
+			<p>Copyright © WIN TECNOLOGIES INC 2018</p>
+		</address>
 	</footer>
 	<script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
 	<script type="text/javascript" src="js/myScript.js"></script>
